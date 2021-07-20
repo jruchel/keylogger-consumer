e31cf70c5a819afcf03dd6be6502ca5c17d479cc
Jakub Ruchel <jruchel254@gmail.com>
2021-07-20 10:42:21 +0200

    initial commit

---
+++ src/main/java/com/example/keyboardinputconsumer/KeyboardInputConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyboardinputconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyboardInputConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyboardInputConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keyboardinputconsumer/KeyboardInputConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keyboardinputconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardInputConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
